ff8828574112a7df12cc385e9e484b29f5a0916c
Enzo Fievet <enzo.fievet@etud.u-picardie.fr>
2026-02-09 19:07:56 +0100

    Chapitre 4 - Partie 4.2

--- UniversiteEFDataProvider/Repositories/NoteRepository.cs
+++ UniversiteEFDataProvider/Repositories/NoteRepository.cs
@@ -1,6 +1,42 @@
+using UniversiteDomain.DataAdapters;
+using UniversiteDomain.Entities;
+using UniversiteEFDataProvider.Data;
+
 namespace UniversiteEFDataProvider.Repositories;
 
-public class NoteRepository
+public class NoteRepository(UniversiteDbContext context) : Repository<Note>(context), INoteRepository
 {
+    public async Task<Note> AddEtudiantAsync(long idNote, long idEtudiant)
+    {
+        ArgumentNullException.ThrowIfNull(Context.Notes);
+        ArgumentNullException.ThrowIfNull(Context.Etudiants);
+        Note n = (await Context.Notes.FindAsync(idNote))!;
+        Etudiant e = (await Context.Etudiants.FindAsync(idEtudiant))!;
+        n.Etudiant = e;
+        n.EtudiantId = idEtudiant;
+        await Context.SaveChangesAsync();
+        return n;
+    }
     
+    public Task<Note> AddEtudiantAsync(Note note, Etudiant etudiant)
+    {
+        return AddEtudiantAsync(note.Id, etudiant.Id);
+    }
+
+    public async Task<Note> AddUeAsync(long idNote, long idUe)
+    {
+        ArgumentNullException.ThrowIfNull(Context.Notes);
+        ArgumentNullException.ThrowIfNull(Context.Ues);
+        Note n = (await Context.Notes.FindAsync(idNote))!;
+        Ue u = (await Context.Ues.FindAsync(idUe))!;
+        n.Ue = u;
+        n.UeId = idUe;
+        await Context.SaveChangesAsync();
+        return n;
+    }
+
+    public Task<Note> AddUeAsync(Parcours note, Ue ue)
+    {
+        return AddEtudiantAsync(note.Id, ue.Id);
+    }
 }
--- UniversiteEFDataProvider/Repositories/ParcoursRepository.cs
+++ UniversiteEFDataProvider/Repositories/ParcoursRepository.cs
@@ -1,6 +1,88 @@
+using UniversiteDomain.DataAdapters;
+using UniversiteDomain.Entities;
+using UniversiteEFDataProvider.Data;
+
 namespace UniversiteEFDataProvider.Repositories;
 
-public class ParcoursRepository
+public class ParcoursRepository(UniversiteDbContext context) : Repository<Parcours>(context), IParcoursRepository
 {
+    public async Task<Parcours> AddEtudiantAsync(long idParcours, long idEtudiant)
+    {
+        ArgumentNullException.ThrowIfNull(Context.Parcours);
+        ArgumentNullException.ThrowIfNull(Context.Etudiants);
+        Parcours p = (await Context.Parcours.FindAsync(idParcours))!;
+        Etudiant e = (await Context.Etudiants.FindAsync(idEtudiant))!;
+        p.Inscrits.Add(e);
+        await Context.SaveChangesAsync();
+        return p;
+    }
     
+    public Task<Parcours> AddEtudiantAsync(Parcours parcours, Etudiant etudiant)
+    {
+        return AddEtudiantAsync(parcours.Id, etudiant.Id);
+    }
+    
+    public async Task<Parcours> AddEtudiantAsync(long idParcours, long[] idEtudiants)
+    {
+        ArgumentNullException.ThrowIfNull(Context.Parcours);
+        ArgumentNullException.ThrowIfNull(Context.Etudiants);
+        Parcours p = (await Context.Parcours.FindAsync(idParcours))!;
+        foreach (long idEtudiant in idEtudiants)
+        {
+            Etudiant e = (await Context.Etudiants.FindAsync(idEtudiant))!;
+            p.Inscrits.Add(e);
+        }
+        await Context.SaveChangesAsync();
+        return p;
+    }
+    
+    public Task<Parcours> AddEtudiantAsync(Parcours parcours, List<Etudiant> etudiants)
+    {
+        long[] IdEtudiants = new long[etudiants.Count()];
+        for (int i = 0; i < etudiants.Count(); i++)
+        {
+                IdEtudiants[i] = etudiants.ElementAt(i).Id;
+        }
+        return AddEtudiantAsync(parcours.Id, IdEtudiants); 
+    }
+    
+    public async Task<Parcours> AddUeAsync(long idParcours, long idUe)
+    {
+        ArgumentNullException.ThrowIfNull(Context.Parcours);
+        ArgumentNullException.ThrowIfNull(Context.Ues);
+        Parcours p = (await Context.Parcours.FindAsync(idParcours))!;
+        Ue u = (await Context.Ues.FindAsync(idUe))!;
+        p.UesEnseignees.Add(u);
+        await Context.SaveChangesAsync();
+        return p;
+    }
+    
+    public Task<Parcours> AddUeAsync(Parcours parcours, Ue ue)
+    {
+        return AddUeAsync(parcours.Id, ue.Id); 
+    }
+    
+    public async Task<Parcours> AddUeAsync(long idParcours, long[] idUes)
+    {
+        ArgumentNullException.ThrowIfNull(Context.Parcours);
+        ArgumentNullException.ThrowIfNull(Context.Ues);
+        Parcours p = (await Context.Parcours.FindAsync(idParcours))!;
+        foreach (long idUe in idUes)
+        {
+            Ue u = (await Context.Ues.FindAsync(idUe))!;
+            p.UesEnseignees.Add(u);
+        }
+        await Context.SaveChangesAsync();
+        return p;
+    }
+    
+    public Task<Parcours> AddUeAsync(Parcours parcours, List<Ue> ues)
+    {
+        long[] IdUes = new long[ues.Count()];
+        for (int i = 0; i < ues.Count(); i++)
+        {
+            IdUes[i] = ues.ElementAt(i).Id;
+        }
+        return AddEtudiantAsync(parcours.Id, IdUes); 
+    }
 }
